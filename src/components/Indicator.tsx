334ef800ef1f954a8f3bf4916b0aac3fd7dcc0b3
codingjordi <jordisaperasc@gmail.com>
2024-12-09 16:02:08 +0100

    fix-replace-inline-styles-for-utility-classes

--- src/components/Indicator.tsx
+++ src/components/Indicator.tsx
@@ -1,14 +1,14 @@
 export default function Indicator({ steps, currentStep }) {
     const printDots = steps.map((_, index) => {
-   return (
-   <span key={index} className={`text-5xl leading-none align-middle dot ${(index === currentStep) ? 'active' : ''}`}>
-    ·
-   </span>
-    );
+        return (
+            <span key={index} className={`text-5xl leading-none align-middle dot ${(index === currentStep) ? 'active' : ''}`}>
+                ·
+            </span>
+        );
     });
-   return (
-   <div className="flex items-center justify-center">
-   {printDots}
-   </div>
+    return (
+        <div className="flex items-center justify-center">
+            {printDots}I
+        </div>
     );
-   }
+}
